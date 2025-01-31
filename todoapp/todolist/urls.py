67e650159e03fe5416ee0085f619155edae21d11
Nekkka <nurievaaa28@gmail.com>
2025-01-31 08:47:20 +0500

    practice3

--- todoapp/todolist/urls.py
+++ todoapp/todolist/urls.py
@@ -9,8 +9,9 @@ urlpattern = [
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todolist.urls'))
+    path('', views.index, name="index"),
+    path('add', views.add, name="add"),
+    path('delete/<int:todo_id>', views.delete, name="delete"),
+    path('update/<int:todo_id>', views.update, name="update"),
 ]
-
 
