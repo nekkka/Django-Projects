36db74899fe4daac37c212ddd0e877d426fbb0b7
Nekkka <nurievaaa28@gmail.com>
2025-02-11 01:12:04 +0500

    cv task additional

--- resume_project/cv/pdf/forms.py
+++ resume_project/cv/pdf/forms.py
@@ -1,6 +1,12 @@
 from django import forms 
+from .models import Contact 
 
-class ContactForm(forms.Form): 
+"""class ContactForm(forms.Form): 
     name = forms.CharField(max_length=100) 
     email = forms.EmailField() 
-    message = forms.CharField(widget=forms.Textarea) 
+    message = forms.CharField(widget=forms.Textarea) """
+
+class ContactForm(forms.ModelForm): 
+    class Meta: 
+        model = Contact 
+        fields = ['name', 'email', 'message'] 
--- resume_project/cv/pdf/views.py
+++ resume_project/cv/pdf/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import Profile
 import pdfkit
 from django.http import HttpResponse
@@ -43,7 +43,7 @@ def list(request):
     profiles = Profile.objects.all()
     return render(request, 'pdf/list.html', {'profiles':profiles})
 
-
+"""
 def contact_view(request):
     if request.method == "POST": 
         form = ContactForm(request.POST) 
@@ -53,6 +53,16 @@ def contact_view(request):
             message = form.cleaned_data['message'] 
             # You can process the form data (e.g., save to DB, send an email) 
             return render(request, 'success.html', {'name': name}) 
+    else: 
+        form = ContactForm() 
+    return render(request, 'contact.html', {'form': form}) 
+"""
+def contact_view(request): 
+    if request.method == "POST": 
+        form = ContactForm(request.POST) 
+        if form.is_valid(): 
+            form.save()  # Saves directly to the database 
+            return redirect('success_page') 
     else: 
         form = ContactForm() 
     return render(request, 'contact.html', {'form': form}) 
