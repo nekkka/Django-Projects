4facaac7e5052dfd1581e5ac82116ed9384f9bd5
Nekkka <nurievaaa28@gmail.com>
2025-02-24 22:53:19 +0500

    project step1

--- project_mini/users/views.py
+++ project_mini/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, RegisterSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
