0b2c69d69277eb91d84bdaea877c2f9aade78455
Nekkka <nurievaaa28@gmail.com>
2025-02-26 18:25:08 +0500

    docker

--- project_mini/project_mini/settings.py
+++ project_mini/project_mini/settings.py
@@ -83,12 +83,14 @@ WSGI_APPLICATION = 'project_mini.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), '..', 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
